01de2694c75e4318f7bd421d567704630f0cf012
Tarık Kılıç Alsancak <batmaca2002@gmail.com>
2024-12-06 11:28:07 +0300

    optimization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   rules: {
     'no-undef': 'off',
+    'no-alert': 'off',
   },
   typescript: true,
   vue: true,
--- stores/telegram.ts
+++ stores/telegram.ts
@@ -5,8 +5,7 @@ export const useTelegramStore = defineStore('telegram', () => {
   const fetchTelegram = async () => {
     const response = await fetch(`https://api.telegram.org/bot${botToken}/getUpdates`)
     const data = await response.json()
-
-    chatId.value = data.result[data.result.length - 1].message.chat.id
+    chatId.value = data.result[data.result.length - 1]?.message.chat.id
   }
 
   const sendUUIDToTelegram = async (uuid: any) => {
